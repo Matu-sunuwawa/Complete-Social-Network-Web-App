bc4cc59d6c3574fb3992a6a8135a29e8d4508bed
MatyaSina <matyassinaadugna@gmail.com>
2026-01-14 23:24:10 +0300

    fixed issues on update and delete and page relpoading issues

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
   path('create/', PostCreateView.as_view(), name="post_create"),
   path('<int:pk>/', PostDetailView.as_view(), name="post_detail"),
   path('<int:pk>/update/', PostUpdateView.as_view(), name="post_update"),
+  path('<int:pk>/delete/', PostDeleteView.as_view(), name="post_delete"),
 
   path('<int:pk>/like/', like_post, name="like_post"),
 ]
--- apps/post/views.py
+++ apps/post/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import (
   ListView, DetailView, CreateView,
-  UpdateView
+  UpdateView, DeleteView
 )
+from django.http import HttpResponse
 from .models import Post
 import time
 
@@ -15,9 +16,18 @@ class PostCreateView(CreateView):
   model = Post
   fields = ['content', 'image']
 
+  def get_template_names(self):
+    if self.request.headers.get('HX-Request'):
+      return ['post/partials/post_create.html']
+
   def form_valid(self, form):
     form.instance.user = self.request.user
     self.object = form.save()
+
+    if self.request.headers.get('HX-Request'):
+        posts = Post.objects.all()
+        return render(self.request, 'core/partials/home.html', {'posts': posts})
+
     return super().form_valid(form)
 
   def get_success_url(self):
@@ -42,6 +52,26 @@ class PostUpdateView(UpdateView):
 
   def get_success_url(self):
     return reverse_lazy('core:home')
+
+class PostDeleteView(DeleteView):
+  model = Post
+
+  def get_template_names(self):
+    if self.request.headers.get('HX-Request'):
+      return ['post/partials/post_delete.html']
+
+  def delete(self, request, *args, **kwargs):
+    self.object = self.get_object()
+    self.object.delete()
+
+    if self.request.headers.get('HX-Request'):
+        return HttpResponse("")
+
+    return super().delete(request, *args, **kwargs)
+
+  def get_success_url(self):
+    return reverse_lazy('core:home')
+
 
 def like_post(request, pk):
   post = get_object_or_404(Post, pk=pk)
